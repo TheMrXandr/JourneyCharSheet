26c932e350c5419a126371ee4ee8c8a76e65f1a5
Roy Hall <HairAndBeardGuy@gmail.com>
2020-11-21 18:37:53 +0000

    feat: Add multiple sets

--- js/main.js
+++ js/main.js
@@ -1,17 +1,89 @@
-$(document).ready(function () {
-  $elements = jQuery("[data-save-load=enabled]");  
+function getWindowHash(){
+  $hash = window.location.hash;
+  if($hash == ''){
+    $hash = '1';
+    window.location.hash = $hash;
+  }
+  console.warn($hash);
+  return $hash.replace('#','');
+}
+function readFromLocalStorage(setid){
+  $data = localStorage.getItem(setid);
+  console.log('reading');
+  console.log(setid);
+  console.table(JSON.parse($data));
+  return JSON.parse($data);
+}
+function writeToLocalStorage(setid,setdata){
+  console.table(setdata);
+  // setdata_json = {setdata}; //JSON.stringify(setdata);
+  setdata_json = JSON.stringify(setdata);
+  console.table(setdata_json);
+  return localStorage.setItem(setid,setdata_json);
+}
+function readSetDataFromForm(){
+  setid = getWindowHash();//jQuery('#main-content').data('setid');
+  $elements = jQuery("[data-save-load=enabled]");
+  $data = {};
   $elements.each(function () {
     var thisOne = $(this);
+    $val = thisOne.val();
+    $id = thisOne.attr("id");
     console.log($(this).val() + " " + $(this).attr("id"));
-    if (localStorage.getItem(thisOne.attr("id"))) {
-      thisOne.val(localStorage.getItem(thisOne.attr("id")));
-    }
+    $data[$id] = $val;
   });
-
+  console.log($data);
+  return $data;
+}
+function writeSetDataToForm(setid,setdata){
+  // jQuery('#main-content').data('setid',setid);
+  window.location.hash = setid;
+  $elements = jQuery("[data-save-load=enabled]");
+  $elements.each(function () {
+    $data = setdata;
+    var thisOne = $(this);
+    $id = thisOne.attr("id");
+    $val = $data[$id];
+    thisOne.val($val);
+    console.log($(this).val() + " " + $(this).attr("id"));
+  });
+  console.log($data);
+  return $data;
+}
+function initSetsPanel(){
+  // jQuery('button.set').remove();
+  for (var i = 0; i < localStorage.length; i++){
+  // for (var i = localStorage.length-1; i >= 0; i--){    
+    // do something with localStorage.getItem(localStorage.key(i));
+    setid = localStorage.key(i);
+    console.log(setid);
+    // jQuery('.sets-panel').prepend('<button class="set" data-setid="'+setid+'">'+setid+'</button>');
+    // jQuery('.sets-panel').append(jQuery('#btn-hide-sets-panel'));
+    jQuery('<button class="set" data-setid="'+setid+'">'+setid+'</button>').insertBefore('.new-set');
+  }
+}
+$(document).ready(function () {
+  initSetsPanel();
+  $elements = jQuery("[data-save-load=enabled]");  
+  // $elements.each(function () {
+  //   var thisOne = $(this);
+  //   console.log($(this).val() + " " + $(this).attr("id"));
+  //   if (localStorage.getItem(thisOne.attr("id"))) {
+  //     thisOne.val(localStorage.getItem(thisOne.attr("id")));
+  //   }
+  // });
+  setid = getWindowHash();//jQuery('#main-content').data('setid');
+  setdata = readFromLocalStorage(setid);
+  console.log(setdata);
+  if(setdata){
+    $written = writeSetDataToForm(setid,setdata);
+  }
   $elements.on("change", function () {
     var thisOne = $(this);
     console.log("change " + thisOne.attr("id"));
-    localStorage.setItem(thisOne.attr("id"), thisOne.val());
+    // localStorage.setItem(thisOne.attr("id"), thisOne.val());
+    setdata = readSetDataFromForm();
+    $written = writeToLocalStorage(setid,setdata);
     $.toast({
         text: "Updated", // Text that is to be shown in the toast
         showHideTransition: 'fade', // fade, slide or plain
@@ -34,5 +106,67 @@ $(document).ready(function () {
   jQuery("#btn-clear-local-storage").click(function () {
     localStorage.clear();
     location.reload();
+  });
+
+  jQuery("#btn-show-sets-panel").click(function () {
+    sets_panel = jQuery('.sets-panel');
+    sidebar_button = jQuery(this);
+    if(sets_panel.hasClass('closed')){
+      sets_panel.removeClass('closed');
+      sidebar_button.text('Hide Sets Panel');  
+    } else {
+      sets_panel.addClass('closed');
+      sidebar_button.text('Show Sets Panel');        
+    }
+  });
+  jQuery('div.sets-panel').click(function(e){
+    e.preventDefault();
+      jQuery("#btn-show-sets-panel").trigger('click');
+  });
+  jQuery("#btn-hide-sets-panel").click(function () {
+    jQuery("#btn-show-sets-panel").trigger('click');
+  });
+  jQuery('.sets-panel').click(function(e){
+    // e.preventDefault();
+    // e.stopPropagation();
+    console.table(e.target);
+    if(jQuery(e.target).hasClass('set')){
+      console.log('setspanelbutton');
+
+      setid = jQuery(e.target).data('setid');
+      // jQuery('#main-content').data(setid,setid);
+      window.location.hash = setid;
+      console.log('setid: '+setid);
+      setdata = readFromLocalStorage(setid);
+      console.log(setdata);
+      if(setdata){
+        $written = writeSetDataToForm(setid,setdata);
+      }
+      window.setTimeout(function(){
+        // jQuery("#btn-show-sets-panel").trigger('click');
+      },100);
+    // console.log(jQuery(this)).attr('class'));
+    // console.log('button.set');
+  }
+  });
+  jQuery('button.new-set').click(function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    // setid = alert('Enter Set ID');
+    setid = jQuery('button.set').length + 1;
+    jQuery('<button class="set" data-setid="'+setid+'">'+setid+'</button>').insertBefore('.new-set');
+    window.location.hash = ''+setid;
+    $elements = jQuery("[data-save-load=enabled]");
+    $elements.each(function () {
+      $data = setdata;
+      var thisOne = $(this);
+      $id = thisOne.attr("id");
+      // $val = $data[$id];
+      // thisOne.val($val);
+      console.log($(this).val() + " " + $(this).attr("id"));
+      thisOne.val('');
+    });
+    setdata = readSetDataFromForm();
+    $written = writeToLocalStorage(setid,setdata);
   });
 });
